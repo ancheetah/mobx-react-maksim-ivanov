9cfc6e615a78de97782e8d1bd98022d7014b7314
AJ <ancheetah@users.noreply.github.com>
2023-01-12 20:49:30 -0500

    remove note functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React from 'react';
 import './App.css';
 import { NewNoteForm } from './NewNoteForm';
 import { useNotesStore } from './NotesContext';
 import { useObserver } from 'mobx-react';
 
 function App() {
-  const notesStore = useNotesStore()
+    const notesStore = useNotesStore();
 
-  // Make the component observable with useObserver hook
-  return useObserver( () => (
-    <>
-      <ul>
-        {notesStore.notes.map(note => (
-          <li key={note.id}>{note.text}</li>
-        ))}
-      </ul>
-      <NewNoteForm/>
-    </>
-  ));
+    // Make the component observable with useObserver hook
+    return useObserver(() => (
+        <>
+            <h1>Notes</h1>
+            <ul>
+                {notesStore.notes.map((note) => (
+                    <li key={note.id}>
+                        {note.text}{' '}
+                        <span onClick={() => notesStore.removeNote(note.id)}>
+                            [X]
+                        </span>
+                    </li>
+                ))}
+            </ul>
+            <NewNoteForm />
+        </>
+    ));
 }
 
 export default App;
