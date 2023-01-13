3902974085de5e34fe821ecc812ee2536c9d732b
AJ <ancheetah@users.noreply.github.com>
2023-01-12 20:15:05 -0500

    create context for notes

---
+++ src/NotesContext.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createNotesStore } from './notesStore'
+import { useLocalStore } from 'mobx-react'  // make Notes Store observable
+
+const NotesContext = React.createContext(null) // this context has a Provider and Consumer
+
+export const NotesProvider = ({children}) => {
+    const notesStore = useLocalStore(createNotesStore())
+
+    return <NotesContext.Provider value={notesStore}>
+        {children}
+    </NotesContext.Provider>
+}
+
+// Create a hook to use/consume the context
+export const useNotesStore = () => React.useContext(NotesContext)
